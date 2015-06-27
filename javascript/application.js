32e639be00ce5516298d7e2245cdc21be913c078
shuhei <kawashu0723@gmail.com>
2015-06-27 17:42:57 +0900

    spec3

--- javascript/application.js
+++ javascript/application.js
@@ -1,5 +1,14 @@
 window.onload = function(){
+  // svgを表示
   d3.xml("svg/BlankMap-World6.svg", "image/svg+xml", function(xml) {
     document.body.appendChild(xml.documentElement);
+  });
+
+  // 国の一覧を表示
+  d3.json("json/population.json", function(error, json){
+    for(var i=0; i<json[1].length; i++){
+      var text = json[1][i].country.value;
+      console.log(text);
+    }
   });
 };
